61755613be9d4767e80c997b4b5439aebefed9b8
大山 <dashan@dashandeMacBook-Pro.local>
2025-12-18 18:22:58 +0800

    add

---
+++ app/register/layout.tsx
@@ -0,0 +1,20 @@
+import { AuthProvider } from '@/components/auth-provider'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import { createServerClient } from '@/lib/connect'
+
+// const getStaticProps = (async () => {
+//   const supabase = await createServerClient()
+//   return { props: { supabase } }
+// }) satisfies GetStaticProps<any>
+
+export default function LoginLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return <>
+   <AuthProvider>
+        <section style={{'background': 'red'}}>{children}</section>
+   </AuthProvider>
+  </>
+}
